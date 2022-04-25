58c2646c3a0dea94b606c8cebbeb1c52b0c3fb2f
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-25 19:27:06 -0300

    Adicionado form.html

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 Copyright (c) 2022 - present
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='apps/templates')
 
@@ -30,6 +30,16 @@ def hello_world():
 def subscribe():
     title = 'Subscribe'
     return render_template('subscribe.html', title=title)
+
+
+@app.route('/form', methods=["POST"])
+def form():
+    title = 'Obrigado!'
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    email = request.form.get('email')
+    print(first_name, last_name, email)
+    return render_template('form.html', title=title)
 
 
 if __name__ == '__main__':
