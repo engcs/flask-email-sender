d440f9f99babeed244a92493be8b3a2b6ef33737
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-27 23:09:35 -0300

    Atualização de print_table.py para SQLAlchemy

--- scripts/print_table.py
+++ scripts/print_table.py
@@ -1,34 +1,35 @@
 """
 ESTE SCRIPT SERVE APENAS PARA FINS DIDÁTICOS
 """
-from sqlalchemy import create_engine
-# engine = create_engine('sqlite:///subs.db', echo=False)
+# import sqlite3 as lite
+# conn = lite.connect('subs.db')
+# cur = conn.cursor()
+# def get_posts():
+#     with conn:
+#         cur.execute("SELECT * FROM Subscribers")
+#         return cur.fetchall()
 
-import sqlite3 as lite
 from rich.console import Console
 from rich.table import Table
+from sqlalchemy import create_engine, MetaData, Table as sqlTable, select
 
-conn = lite.connect('subs.db')
-cur = conn.cursor()
+engine = create_engine('sqlite:///subs.db', echo=False)
+connection = engine.connect()
+metadata = MetaData()
+show = sqlTable('Subscribers', metadata, autoload=True, autoload_with = engine)
+query = select([show])
+result_proxy = connection.execute(query)
+result_set = result_proxy.fetchall()
+data = result_set
 
-
-def get_posts():
-    with conn:
-        cur.execute("SELECT * FROM Subscribers")
-        return cur.fetchall()
-
-
-data = get_posts()
 table = Table(title="Subscribers")
 
 table.add_column("Id", justify="left", style="cyan", no_wrap=True)
 table.add_column("Name", justify="left", style="magenta", no_wrap=True)
 table.add_column("Date Created", justify="left", style="green", no_wrap=True)
 
-
 for row in data:
     table.add_row(*[str(cell) for cell in row])
-
 
 console = Console()
 console.print(table)
