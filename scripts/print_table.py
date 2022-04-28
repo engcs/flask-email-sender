31e0245c14b33b431200cc028f5577cc801a75d1
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-28 00:15:26 -0300

    Renomeado método Table do SQLAlchemy

--- scripts/print_table.py
+++ scripts/print_table.py
@@ -11,22 +11,24 @@ ESTE SCRIPT SERVE APENAS PARA FINS DIDÁTICOS
 
 from rich.console import Console
 from rich.table import Table
-from sqlalchemy import create_engine, MetaData, Table as sqlTable, select
+from sqlalchemy import MetaData
+from sqlalchemy import Table as sqlTable
+from sqlalchemy import create_engine, select
 
 engine = create_engine('sqlite:///subs.db', echo=False)
 connection = engine.connect()
 metadata = MetaData()
-show = sqlTable('Subscribers', metadata, autoload=True, autoload_with = engine)
+show = sqlTable('Subscribers', metadata, autoload=True, autoload_with=engine)
 query = select([show])
 result_proxy = connection.execute(query)
 result_set = result_proxy.fetchall()
 data = result_set
 
-table = Table(title="Subscribers")
+table = Table(title='Subscribers')
 
-table.add_column("Id", justify="left", style="cyan", no_wrap=True)
-table.add_column("Name", justify="left", style="magenta", no_wrap=True)
-table.add_column("Date Created", justify="left", style="green", no_wrap=True)
+table.add_column('Id', justify='left', style='cyan', no_wrap=True)
+table.add_column('Name', justify='left', style='magenta', no_wrap=True)
+table.add_column('Date Created', justify='left', style='green', no_wrap=True)
 
 for row in data:
     table.add_row(*[str(cell) for cell in row])
