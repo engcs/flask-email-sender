4557c23d973bbd9041043c722d0e6c529dfe8adb
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-27 22:51:23 -0300

    Alteração em script de impressão de Subscribers

--- scripts/print_table.py
+++ scripts/print_table.py
@@ -1,10 +1,34 @@
 """
 ESTE SCRIPT SERVE APENAS PARA FINS DIDÁTICOS
 """
-import pandas as pd
 from sqlalchemy import create_engine
+# engine = create_engine('sqlite:///subs.db', echo=False)
 
-engine = create_engine('sqlite:///subs.db', echo=False)
-table = pd.read_sql_table(table_name='subscribers', con=engine)
+import sqlite3 as lite
+from rich.console import Console
+from rich.table import Table
 
-print(table)
+conn = lite.connect('subs.db')
+cur = conn.cursor()
+
+
+def get_posts():
+    with conn:
+        cur.execute("SELECT * FROM Subscribers")
+        return cur.fetchall()
+
+
+data = get_posts()
+table = Table(title="Subscribers")
+
+table.add_column("Id", justify="left", style="cyan", no_wrap=True)
+table.add_column("Name", justify="left", style="magenta", no_wrap=True)
+table.add_column("Date Created", justify="left", style="green", no_wrap=True)
+
+
+for row in data:
+    table.add_row(*[str(cell) for cell in row])
+
+
+console = Console()
+console.print(table)
