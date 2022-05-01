3097a850dac7be6d9257abce4346dfe5d84ff106
Cristian Sousa <eng.cristiansousa@gmail.com>
2022-04-30 21:43:12 -0300

    Adicionado script.js

---
+++ apps/static/js/script.js
@@ -0,0 +1,31 @@
+function showAlert(){
+  $("#barra_de_alerta").append(`
+    <div class="alert alert-warning alert-dismissible teste fade show" style="display:show" role="alert" id="myAlert">
+        <strong>Holy guacamole!</strong>
+          <span id="myAlert2">
+            You should check in on some of those fields below.
+          </span>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `);
+  $.each($('.teste'), function(index, element) {
+    var t = setTimeout(function() {
+        element.setAttribute('style', 'display:none');
+        //$(".alert").alert('close');
+        console.log(getAllMethods(element))
+    }, 3000);
+  });
+
+}
+
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  var button = event.relatedTarget
+  var recipient = button.getAttribute('data-id')
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('#meu_id')
+  modalTitle.textContent = 'Deletar registro ' + recipient
+  modalBodyInput.value = recipient
+  var btn = document.getElementById('btn-delete')
+  btn.href = '/delete/' + recipient
+})
